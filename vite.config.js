e79a0c2174f3ec595f2532c4462f8a21fcf52c04
Kier Daniel Tavas <kian.tavas15g@gmail.com>
2025-07-16 15:36:00 +0800

    edited package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'kdt-portfolio',
+  base: '/kdt-portfolio',
   plugins: [react(), tailwindcss()],
 })
